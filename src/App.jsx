d3d9ede7d9e8b36b4e603ac7504aee10e59bcb00
Ola Mobolaji <mobolajiola22@gmail.com>
2024-07-07 00:35:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CheckOut from "./Checkout";
+
+
+function App() {
+  return (
+    <>
+      <CheckOut />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Store.jsx
@@ -0,0 +1,9 @@
+const Store = () => {
+    return (
+        <div className="store">
+            
+        </div>
+    );
+}
+ 
+export default Store;
